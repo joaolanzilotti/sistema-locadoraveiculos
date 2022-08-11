6abfa51ad56349babd720687c6e293c50cc12776
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-08-11 19:16:36 -0300

    v1

---
+++ target/generated-sources/annotations/modelo/Fabricante_.java
@@ -0,0 +1,14 @@
+package modelo;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2022-08-11T19:16:26", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Fabricante.class)
+public class Fabricante_ { 
+
+    public static volatile SingularAttribute<Fabricante, Long> codigo;
+    public static volatile SingularAttribute<Fabricante, String> nome;
+
+}
